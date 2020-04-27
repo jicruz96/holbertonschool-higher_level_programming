1e5f49993e7a7c50cb48e368a51c48e911936f09
jicruz96 <accounts@jicruz.com>
2020-04-27 19:10:32 +0000

    Push 10-check_cycle.c to repo along with its helper files

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks for a loop in a linked list
+ * @list: linked list to check
+ * Return: 1 if loop found | 0 if no loop found
+ **/
+int check_cycle(listint_t *list)
+{
+	listint_t *p1, *p2;
+
+	if (list)
+		for (p2 = list->next; p2; p2 = p2->next)
+			for (p1 = list; p1 != p2 || p2 == list; p1 = p1->next)
+				if (p1 == p2 || p2 == list)
+					return (1);
+
+	return (0);
+}
