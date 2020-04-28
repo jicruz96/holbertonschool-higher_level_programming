1e159ea6d67929f5aece508c2a65843450b7125c
jicruz96 <accounts@jicruz.com>
2020-04-28 22:18:50 +0000

    Change string formatting in line 8 of file 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(len(str)):
-        if ord(str[i]) > 96 and ord(str[i]) < 123:
-            print('{:c}'.format((ord(str[i]) - 32)), end='')
+        j = ord(str[i])
+        if j > 96 and j < 123:
+            print('{:c}'.format(j - 32), end='')
         else:
-            print(str[i], end='')
+            print('{:c}'.format(j), end='')
     print()
