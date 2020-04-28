467e5592cf4a621142105c5b199bee4e7ee58106
jicruz96 <accounts@jicruz.com>
2020-04-28 04:53:35 +0000

    Revert to old version for ex. 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,19 +1,19 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
  * check_cycle - checks for a loop in a linked list
  * @list: linked list to check
  * Return: 1 if loop found | 0 if no loop found
  **/
-
 int check_cycle(listint_t *list)
 {
-	listint_t *p1;
+	listint_t *p1, *p2;
 
-	for (; list; list = list->next)
-		for (p1 = list->next; p1; p1 = p1->next)
-			if (list == p1)
-				return (1);
+	if (list)
+		for (p2 = list->next; p2; p2 = p2->next)
+			for (p1 = list; p1 != p2 || p2 == list; p1 = p1->next)
+				if (p1->next == p2->next)
+					return (1);
+
 	return (0);
 }
