820299bfb1fae3d8ccf7bf2d99062fc456fdb92d
jicruz96 <accounts@jicruz.com>
2020-04-28 22:09:22 +0000

    Change string formatting in file 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(8):
     for j in range(i + 1, 10):
-        print(i, end='')
-        print(j, end=', ')
+        print('{:d}'.format(i), end='')
+        print('{:d}'.format(j), end=', ')
 print('89')
