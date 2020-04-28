add7d62f339c654de9d5242e692ed542d330c784
jicruz96 <accounts@jicruz.com>
2020-04-28 22:20:41 +0000

    refactor file 8 so only two print statements are needed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,6 @@ def uppercase(str):
     for i in range(len(str)):
         j = ord(str[i])
         if j > 96 and j < 123:
-            print('{:c}'.format(j - 32), end='')
-        else:
-            print('{:c}'.format(j), end='')
+            j -= 32
+        print('{:c}'.format(j), end='')
     print()
