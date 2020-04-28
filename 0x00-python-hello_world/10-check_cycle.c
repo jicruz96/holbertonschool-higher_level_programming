ef9855000f73f552af4e3c76dca195527e06341c
jicruz96 <accounts@jicruz.com>
2020-04-28 08:03:20 +0000

    Make 10-check_cycle.c comply with style requirements

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,15 +10,15 @@ int check_cycle(listint_t *list)
 	listint_t *p1;
 
 	if (list)
+	{
 		for (p1 = list->next; p1; list = list->next, p1 = p1->next)
 		{
 			if (list->next == p1->next)
 				return (1);
-			
 			p1 = p1->next;
 			if (!p1)
 				break;
 		}
-
+	}
 	return (0);
 }
