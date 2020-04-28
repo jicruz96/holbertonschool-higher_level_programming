84cf89ad5dc49309192f8437455a0c8e555c0e36
jicruz96 <accounts@jicruz.com>
2020-04-28 04:04:11 +0000

    Change some of the logic tests in 10-check_cycle to improve program efficiency

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,13 +7,11 @@
  **/
 int check_cycle(listint_t *list)
 {
-	listint_t *p1, *p2;
+	listint_t *p1 = list, *p2 = list;
 
-	if (list)
-		for (p2 = list->next; p2; p2 = p2->next)
-			for (p1 = list; p1 != p2 || p2 == list; p1 = p1->next)
-				if (p1->next == p2->next || p2 == list)
-					return (1);
-
+	while (p1->next)
+		for (p2 = p1->next, p1 = list; p1 != p2  || p2 == list; p1 = p1->next)
+			if (p1->next == p2->next)
+				return (1);
 	return (0);
 }
