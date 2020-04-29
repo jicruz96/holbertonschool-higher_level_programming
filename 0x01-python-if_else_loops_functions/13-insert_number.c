c48b6812ca2fa6250249f57452ab5d08cb333621
jicruz96 <accounts@jicruz.com>
2020-04-29 02:15:33 +0000

    Change algorithm in 13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -9,43 +9,28 @@
  **/
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new_node, *ptr, *prev;
+	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *ptr = *head;
+	listint_t *prev = *head;
 
-	/* Return NULL if head pointer is NULL (i.e. empty list) */
-	if (head == NULL || *head == NULL)
-		return (NULL);
-
-	/* Allocate memory for new struct */
-	new_node = malloc(sizeof(listint_t));
-
-	/* If memory allocation failed, return NULL */
-	if (new_node == NULL)
-		return (NULL);
-
-	/* Store number into the node */
-	new_node->n = number;
-
-	/* Initialize our pointers to point to head node */
-	ptr = *head;
-	prev = *head;
-
-	/* Traverse through the list */
-	while (ptr != NULL)
+	if (new_node != NULL)
 	{
-		if (ptr->n >= number)
+		while (ptr != NULL || prev != NULL)
 		{
-			prev->next = new_node;
-			new_node->next = ptr;
-			return (new_node);
+			if (ptr == NULL || ptr->n >= number)
+			{
+				new_node->n = number;
+				if (ptr == *head)
+					*head = new_node;
+				else
+					prev->next = new_node;
+				new_node->next = ptr;
+				return (new_node);
+			}
+			prev = ptr;
+			ptr = ptr->next;
 		}
-		prev = ptr;
-		ptr = ptr->next;
+		free(new_node);
 	}
-	/**
-	 * If we're here, then number is the smallest number
-	 * in the list. Set new_node as the new head node
-	 **/
-	new_node->next = *head;
-	*head = new_node;
-	return (*head);
+	return (NULL);
 }
