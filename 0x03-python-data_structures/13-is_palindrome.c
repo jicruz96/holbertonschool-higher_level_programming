618ca672803993d286ee388f7accb61a70f138da
jicruz96 <accounts@jicruz.com>
2020-05-06 01:19:10 +0000

    Remove print statements in file 13 that were meant for debugging purposes

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -17,16 +17,9 @@ int is_palindrome(listint_t **head)
 	for (tmp = *head; tmp != NULL; tmp = tmp->next)
 		array[i++] = tmp->n;
 
-	if (j > (i - 1) / 2)
-		printf("NO LOOP FOR YOU\n");
-	else
-		printf("LOOP TIME!\n");
-
 	for (len = i - 1; j <= len / 2; j++)
-	{
-		printf("\tarray[%d] = %d\tarray[%d] = %d\n", j, array[j], len - j, array[len - j]);
 		if (array[j] != array[len - j])
 			return (0);
-	}
+
 	return (1);
 }
