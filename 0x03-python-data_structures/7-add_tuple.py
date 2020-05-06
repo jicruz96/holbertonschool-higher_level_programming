26dbeeff2fca7fcd0fab351d17a8d5801ca3f939
jicruz96 <accounts@jicruz.com>
2020-05-06 03:27:50 +0000

    Ensure file 7 can handle inputs of tuples with more than 2 arguments

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     args = [tuple_a, tuple_b]
-    vals = [0, 0]
+    vals = [0 for i in args]
     for i in args:
         for j in range(len(i)):
             vals[j] = vals[j] + i[j]
-    new_tuple = (vals[0], vals[1])
-
-    return new_tuple
+            if j == 1:
+                break
+    return vals[0], vals[1]
