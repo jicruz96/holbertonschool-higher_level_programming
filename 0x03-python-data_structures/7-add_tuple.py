b31fe29cb2704686ec5d1e959f36b6d4c56b4f0b
jicruz96 <accounts@jicruz.com>
2020-05-06 03:41:31 +0000

    change dynamic list to static list in file 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     args = [tuple_a, tuple_b]
-    vals = [0 for i in args]
+    vals = [0, 0]
     for i in args:
         for j in range(len(i)):
             vals[j] = vals[j] + i[j]
