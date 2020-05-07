69702d552313645b5464f8b53466900bed066552
jicruz96 <accounts@jicruz.com>
2020-05-07 02:56:53 +0000

    Alter algorithm for 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    args = [tuple_a, tuple_b]
-    vals = [0, 0]
-    for i in args:
-        for j in range(len(i)):
-            vals[j] = vals[j] + i[j]
-            if j == 1:
-                break
-    return vals[0], vals[1]
+    list = [tuple_a, tuple_b]
+    sum = [0, 0]
+    for i in list:
+        if len(i):
+            sum[0] += i[0]
+            if len(i) > 1:
+                sum[1] += i[1]
+    return sum[0], sum[1]
