dc9ad36f50c8f27d3389c76c52155d7aea3d6cfb
jicruz96 <accounts@jicruz.com>
2020-05-08 22:31:57 +0000

    add comments to file 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -12,7 +12,7 @@ def roman_to_int(roman_string):
 
 
 """
-ALTERNATE VERSION: LOOPED   -   slightly more readable
+ALTERNATE VERSION: LOOPED
 def roman_to_int(roman_string):
     d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     prev = 0
