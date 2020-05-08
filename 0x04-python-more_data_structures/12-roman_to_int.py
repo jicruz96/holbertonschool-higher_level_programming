13c8369cef64421ee0173cdf70a3a15cd8cdbc5c
jicruz96 <accounts@jicruz.com>
2020-05-08 03:26:35 +0000

    Change file 9 so logic returns copy of input using comprehension; change return and factoring in file 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or roman_string is "":
-        return None
-
-    # Variables and Numeral Values Dictionary:
     val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     prev = 0
     sum = 0
-
-    # Go through each letter in the string
-    for i in list(roman_string):
-        if val[i] > prev:
-            prev *= -1
-        sum += prev
-        prev = val[i]
-
+    if roman_string:
+        for i in list(roman_string):
+            if val[i] > prev:
+                prev *= -1
+            sum += prev
+            prev = val[i]
     return sum + prev
--- 0x04-python-more_data_structures/helper_files/12-main.py
+++ 0x04-python-more_data_structures/helper_files/12-main.py
@@ -3,17 +3,11 @@
 """
 roman_to_int = __import__('12-roman_to_int').roman_to_int
 
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+def test_roman(roman_number):
+    return "{} = {}".format(roman_number, roman_to_int(roman_number))
 
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+testlist = ["XX", "XXXIX", "XLIX", "MMMCMXCIX", "", None]
 
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+print(list(map(test_roman, testlist)))
