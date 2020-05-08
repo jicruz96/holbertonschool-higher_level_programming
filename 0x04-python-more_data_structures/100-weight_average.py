3cec5018bac2bb114a9b8c05a5ad6c7d6039f2b5
jicruz96 <accounts@jicruz.com>
2020-05-08 23:21:43 +0000

    Ensure 100-weight_average.py can handle empty lists

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    basic = sum([x[1] for x in my_list])
-    weighted = sum([x[0] * x[1] for x in my_list])
-    return weighted / basic
+    if my_list:
+        basic = sum([x[1] for x in my_list])
+        weighted = sum([x[0] * x[1] for x in my_list])
+        return weighted / basic
+    return 0
