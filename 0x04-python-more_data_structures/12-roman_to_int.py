e1d00853f26de929f8266451a5f3b13f57096bca
jicruz96 <accounts@jicruz.com>
2020-05-08 22:27:30 +0000

    Turn file 12 into recursive function, turn 1 into list comprehension algorithm

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    s = roman_string                # for readability only
+    if s and type(s) is str:
+        n = d[s[0]]                 # n = value of 1st letter
+        rest = roman_to_int(s[1:])  # rest = value of rest of string
+        if s[1:] and n < d[s[1]]:   # if n < value of next letter, negate n
+            n = -n
+        return n + rest             # return total value
+    return 0
+
+
+"""
+ALTERNATE VERSION: LOOPED   -   slightly more readable
+def roman_to_int(roman_string):
+    d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     prev = 0
     sum = 0
     if type(roman_string) is str:
         for i in roman_string:
-            if val[i] > prev:
+            if d[i] > prev:
                 prev *= -1
             sum += prev
-            prev = val[i]
+            prev = d[i]
     return sum + prev
+"""
