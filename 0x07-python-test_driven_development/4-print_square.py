c0ce82bc4c75e7e9aa54847cb94ea2fffd8abdf6
jicruz96 <accounts@jicruz.com>
2020-05-25 23:52:11 +0000

    Remove main-guarded code snippet from file 3, push first version of file 4 (and /test/4.txt)

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -4,3 +4,9 @@
 
 def print_square(size):
     """ Prints a square of length size using the hash (#) character """
+    if type(size) != int:
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    for i in range(size):
+        print(size * '#')
