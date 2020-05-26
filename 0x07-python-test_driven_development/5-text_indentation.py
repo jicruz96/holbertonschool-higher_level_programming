fad0a42daf350cc539fc28efd32aa133af67b814
jicruz96 <accounts@jicruz.com>
2020-05-26 01:11:26 +0000

    Push first version of file 5 (and test file 5)

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,9 +1,29 @@
 #!/usr/bin/python3
-""" 
-This module defines text_indentation, a function that replaces the
-'?', '.', ':' characters with two newlines.
-"""
+""" Defines the text_indentation function """
 
 
 def text_indentation(text):
-    """ Replaces the '?', ',', ':' characters with two newlines """
+    """ Adds two newlines after the '?', ',', ':' characters """
+
+    if type(text) != str:
+        raise TypeError('text must be a string')
+
+    delims = [".", "?", ":"]
+    output = []
+    ignore_spaces = False
+    a_space = " "
+
+    for char in list(text):
+
+        if char in delims:
+            print(char + "\n")
+            ignore_spaces = True
+            continue
+
+        if ignore_spaces is True:
+            if char is a_space:
+                continue
+            else:
+                ignore_spaces = False
+
+        print(char, end='')
