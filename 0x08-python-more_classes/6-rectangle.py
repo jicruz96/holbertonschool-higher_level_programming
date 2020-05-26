3b356b9f3743100f4b1d41152154cb11958341d6
jicruz96 <accounts@jicruz.com>
2020-05-26 21:13:18 +0000

    Push first version of file 6 to repo

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,2 +1,97 @@
 #!/usr/bin/python3
+""" This module defines the Rectangle class """
 
+
+class Rectangle:
+    """ Defines a rectangle with a width and height value
+
+    Args:
+        width (int): width of rectangle
+        height (int): height of rectangle
+
+    Attributes:
+        number_of_instances (int): number of instances of the class
+            in existence
+    """
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """ int: defines width of rectangle.
+
+        Note:
+            width must be an int and above 0
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError('width must be an integer')
+
+        if value < 0:
+            raise ValueError('width must be >= 0')
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """ int: defines height of rectangle.
+
+        Note:
+            height must be an int and above 0
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError('height must be an integer')
+
+        if value < 0:
+            raise ValueError('height must be >= 0')
+
+        self.__height = value
+
+    def area(self):
+        """ returns the rectangle area """
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """ returns the rectangle perimeter """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """ returns string representation of class """
+        str = ""
+        rows = self.__height
+        hashes = self.__width
+        if rows == 0 or hashes == 0:
+            return str
+
+        for row in range(rows):
+            for hash in range(hashes):
+                str += '#'
+            if row != rows - 1:
+                str += '\n'
+        return str
+
+    def __repr__(self):
+        """ returns string representation of command
+        that instantiated the object
+        """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """prints a goodbye message upon deletion of object"""
+        print('Bye rectangle...')
+        Rectangle.number_of_instances -= 1
+        del self
