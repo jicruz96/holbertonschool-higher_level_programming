ca87da3674ca38e6fd5cb42d19152ac79bfc2000
jicruz96 <accounts@jicruz.com>
2020-05-27 02:46:58 +0000

    Clean up file 19

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -3,16 +3,5 @@
 
 
 def copy_list(l):
+    """ Returns a copy of l """
     return l.copy()
-
-
-my_list = [1, 2, 3]
-print(my_list)
-
-new_list = copy_list(my_list)
-
-print(my_list)
-print(new_list)
-
-print(new_list == my_list)
-print(new_list is my_list)
