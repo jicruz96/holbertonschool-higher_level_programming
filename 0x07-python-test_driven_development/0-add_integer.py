06424090aa6e6a41876ac76b8f55b360861bb4df
jicruz96 <accounts@jicruz.com>
2020-05-27 14:35:46 +0000

    Add float overflow test case to test file 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,15 +5,14 @@ This module defines add_integer, a function that returns the sum of 2 integers
 
 
 def add_integer(a, b=98):
-    """
-    Returns sum of two numbers.
+    """Returns sum of two numbers.
     - If one number is inputted, it returns the number plus 98.
     - Floats are converted to integers before addition.
     - Returns a Type Error if an argument is not an int or float.
     """
-    if type(a) is float or type(a) is int:
-        if type(b) is float or type(b) is int:
-            return int(a) + int(b)
+    if type(a) in (float, int):
+        if type(b) in (float, int):
+            return int(a + b)
         raise TypeError('b must be an integer')
 
     raise TypeError('a must be an integer')
