4a552e2a61b226e8d697fdf9387c1ede432b5d0b
jicruz96 <accounts@jicruz.com>
2020-06-12 05:59:12 +0000

    update repo

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,18 +4,30 @@ from models.base import Base
 from models.rectangle import Rectangle
 
 
-class Base_Tests(unittest.TestCase):
+class test_base(unittest.TestCase):
     """ Tests for the base class"""
 
     @classmethod
-    def setUpClass(cls):
-        print('setUp')
-        cls.init = Base()
-        cls.dicts1 = cls.init.__dict__
-        cls.string = cls.init.to_json_string([cls.dicts1])
-        cls.file = 'Base.json'
-        cls.dicts2 = cls.init.from_json_string(cls.string)
+    def setUpClass(self):
+        print('Start up!')
+        self.test = Base()
 
     def test_base_init(self):
         """Checks instantiation of Base Class"""
-        self.assertEqual(self.init.id, 1)
+        self.assertEqual(self.test.id, 1)
+        self.assertEqual(Base().id, 2)
+        self.assertEqual(Base(50).id, 50)
+
+    def test_to_json_string(self):
+        """Checks to_json_string method of Base Class"""
+        list_dict = [Base(1).__dict__]
+        self.assertEqual(Base.to_json_string(list_dict), '[{"id": 1}]')
+        self.assertEqual(Base.to_json_string(None), '[]')
+        self.assertEqual(Base.to_json_string([]), '[]')
+        self.assertEqual(Base.to_json_string([{'id': 12}]), '[{"id": 12}]')
+
+    def test_from_json_string(self):
+        """Checks from_json_string method of Base Class"""
+        self.assertEqual(Base.from_json_string(None), [])
+        self.assertEqual(Base.from_json_string('[]'), [])
+        self.assertEqual(Base.from_json_string('[{"id": 1}]'), [{"id": 1}])
