3bb36270ab048c87affe92ecae7071a671c31aec
jicruz96 <accounts@jicruz.com>
2020-07-16 01:16:29 +0000

    Push final version of file 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,10 @@
 -- SQL INTRO 16
 -- lists all records of second_table with a name
-SELECT score, COUNT(score) AS number
-FROM second_table
-GROUP BY score
-ORDER BY number DESC;
+SELECT
+    score,
+    name
+FROM
+    second_table
+WHERE 
+    name IS NOT NULL
+ORDER BY score DESC;
