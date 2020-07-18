23ec58cc33fbdf179130b71e8a4edcc168d3730e
jicruz96 <accounts@jicruz.com>
2020-07-18 23:56:15 +0000

    Push file 10 to repo

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,6 @@
 -- Cities by State (join)
--- List all cities (and their state) from hlbtn_0d_usa database
-SELECT
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT ts.title, tsg.genre_id
+FROM tv_shows AS ts, tv_show_genres AS tsg
+WHERE ts.id = tsg.show_id
+ORDER BY title, genre_id;
