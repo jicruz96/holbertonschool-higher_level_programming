9ccfa93ffc0e1cb90a518cabb5470706e03992d6
jicruz96 <accounts@jicruz.com>
2020-07-18 23:58:03 +0000

    Add file 13 to repo

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,3 +1,9 @@
--- Cities by State (join)
--- List all cities (and their state) from hlbtn_0d_usa database
+-- Count shows by genre
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
 SELECT
+    tg.name as genre,
+    tsg.genre_id as number_of_shows
+FROM tv_genres as tg, tv_show_genres as tsg
+WHERE tg.id = tsg.genre_id
+GROUP BY tg.id
+ORDER BY number_of_shows DESC;
