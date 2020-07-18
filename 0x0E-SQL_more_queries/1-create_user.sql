2ec5cf0d07520141eb17db7b959c2caf6fe15533
jicruz96 <accounts@jicruz.com>
2020-07-18 00:39:39 +0000

    Fix file 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Create User
 -- Creates user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT PRIVILEGES ON *.* TO 'user_0d_1@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
