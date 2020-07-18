df010a25339fd0617f4a8e66aeef17922b7beb9a
jicruz96 <accounts@jicruz.com>
2020-07-18 23:58:27 +0000

    Add file 15 to repo

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,3 +1,10 @@
--- Cities by State (join)
--- List all cities (and their state) from hlbtn_0d_usa database
-SELECT
+-- Comedy only
+-- lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT ts.title
+FROM tv_shows as ts
+    RIGHT JOIN tv_show_genres as tsg
+    ON ts.id = tsg.show_id
+    LEFT JOIN tv_genres as tg
+    ON tsg.genre_id = tg.id
+WHERE tg.name = "Comedy"
+ORDER BY title;
