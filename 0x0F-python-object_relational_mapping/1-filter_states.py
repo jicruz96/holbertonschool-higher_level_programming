2bfa76e4de85d8017063a96ab39c0b42bbfbe580
jicruz96 <accounts@jicruz.com>
2020-08-08 20:02:05 +0000

    Push first version of file 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,2 +1,33 @@
 #!/usr/bin/python3
+""" lists all states that start with 'N' from the database hbtn_0e_0_usa """
 
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+
+    try:
+        args = {
+            "user": argv[1],
+            "passwd": argv[2],
+            "db": argv[3],
+            "host": "localhost",  # This is default MySQLdb value
+            "port": 3306        # This is default MySQLdb value
+        }
+        QUERY = "SELECT * FROM states WHERE name REGEXP '^[N].*$'"
+
+        # Connect to database, execute QUERY
+        db_connection = MySQLdb.connect(**args)
+        cursor = db_connection.cursor()
+        cursor.execute(QUERY)
+        tuples = cursor.fetchall()
+
+        # Clean up
+        cursor.close()
+        db_connection.close()
+
+        # Print result
+        for tuple in tuples:
+            print(tuple)
+    except:
+        pass
