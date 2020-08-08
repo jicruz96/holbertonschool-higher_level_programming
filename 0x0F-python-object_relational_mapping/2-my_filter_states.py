0700623faadddc7907e9d5bcac42a27f804d3a4d
jicruz96 <accounts@jicruz.com>
2020-08-08 20:27:16 +0000

    Push first version of file 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,2 +1,32 @@
 #!/usr/bin/python3
+""" lists state_entry from db """
 
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    try:
+        args = {
+            "user": argv[1],
+            "passwd": argv[2],
+            "db": argv[3],
+            "host": "localhost",  # This is default MySQLdb value
+            "port": 3306        # This is default MySQLdb value
+        }
+        QUERY = "SELECT * FROM states WHERE name = '{}'".format(argv[4])
+
+        # Connect to database, execute QUERY
+        db_connection = MySQLdb.connect(**args)
+        cursor = db_connection.cursor()
+        cursor.execute(QUERY)
+        tuples = cursor.fetchall()
+
+        # Clean up
+        cursor.close()
+        db_connection.close()
+
+        # Print result
+        for tuple in tuples:
+            print(tuple)
+    except Exception:
+        raise Exception
