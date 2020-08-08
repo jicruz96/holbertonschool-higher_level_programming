84d77e6036a3c395d43d675153c9c35479da5a73
jicruz96 <accounts@jicruz.com>
2020-08-08 21:29:41 +0000

    Push first version of file 3 to repo

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,2 +1,33 @@
 #!/usr/bin/python3
+""" lists state from db """
 
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    try:
+        args = {
+            "user": argv[1],
+            "passwd": argv[2],
+            "db": argv[3],
+            "host": "localhost",  # This is default MySQLdb value
+            "port": 3306        # This is default MySQLdb value
+        }
+        state = argv[4]
+        QUERY = "SELECT * FROM states WHERE BINARY(name) = %(state)s"
+
+        # Connect to database, execute QUERY
+        db_connection = MySQLdb.connect(**args)
+        cursor = db_connection.cursor()
+        cursor.execute(QUERY, {"state": state})
+        tuples = cursor.fetchall()
+
+        # Clean up
+        cursor.close()
+        db_connection.close()
+
+        # Print result
+        for tuple in tuples:
+            print(tuple)
+    except:
+        pass
