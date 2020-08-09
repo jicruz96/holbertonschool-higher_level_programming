408604a2dfc44b03bf18b8b97242fe85c966e5e5
jicruz96 <accounts@jicruz.com>
2020-08-09 06:39:40 +0000

    Push first version of model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,2 +1,26 @@
 #!/usr/bin/python3
-7-model_state_fetch_all.py
+""" defines State class for MySQLdb integration """
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ 
+    Representation of states table
+
+    Args:
+        * id (int): represents id column
+        * name (str): represents name column
+    """
+
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+    def __init__(self, name, id=None):
+        self.id = id
+        self.name = name
