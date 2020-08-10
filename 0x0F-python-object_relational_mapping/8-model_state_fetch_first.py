b3acb7352bbd105fb1cd9a87f29d4713fdda9820
jicruz96 <accounts@jicruz.com>
2020-08-10 01:33:50 +0000

    fix typo in url on file 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -5,14 +5,13 @@ from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm.session import sessionmaker
-from sqlalchemy.orm import session
 
 if __name__ == "__main__":
     try:
         user = argv[1]
         pw = argv[2]
         db = argv[3]
-        URL = "mysql+mysqldb://{}:{}@localhost/{}.".format(user, pw, db)
+        URL = "mysql+mysqldb://{}:{}@localhost/{}".format(user, pw, db)
 
         engine = create_engine(URL)
 
