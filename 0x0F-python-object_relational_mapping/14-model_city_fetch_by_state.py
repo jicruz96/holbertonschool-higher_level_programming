3d43431d7ca907aedeafbdfa7c5641187b10bf4b
jicruz96 <accounts@jicruz.com>
2020-08-10 03:46:11 +0000

    Push model_city.py and file 14 to repo

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,2 +1,29 @@
 #!/usr/bin/python3
+""" prints all City objects from db """
 
+from sys import argv
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm.session import sessionmaker
+
+if __name__ == "__main__":
+
+    state_id_target = 2
+    new_state_name = "New Mexico"
+    user = argv[1]
+    pw = argv[2]
+    db = argv[3]
+    URL = "mysql+mysqldb://{}:{}@localhost/{}".format(user, pw, db)
+
+    engine = create_engine(URL)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for city, state in session\
+            .query(City, State).filter(State.id == City.state_id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,2 +1,26 @@
 #!/usr/bin/python3
+""" defines City class for MySQLdb integration """
 
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql.schema import ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """
+    Representation of cities table
+
+    Args:
+        * id (int): represents id column
+        * name (str): represents name column
+    """
+
+    __tablename__ = "cities"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
+
+    def __init__(self, name, id=None):
+        self.id = id
+        self.name = name
