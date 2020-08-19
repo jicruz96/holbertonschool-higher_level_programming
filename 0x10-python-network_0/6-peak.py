4c113616ffffaf9175478cd7a5297fd781112f9c
jicruz96 <accounts@jicruz.com>
2020-08-19 02:05:37 +0000

    Push alternate algorithm for task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,11 +5,37 @@
 def find_peak(list_of_integers):
     """ returns a peak within a list of integers """
 
-    if list_of_integers is None or len(list_of_integers) is 0:
-        return None
-
-    max = list_of_integers[0]
+    prev = None
     for int in list_of_integers:
-        if int > max:
-            max = int
-    return max
+        if prev is not None and prev >= int:
+            return prev
+        prev = int
+    return prev
+
+    # num_of_ints = len(list_of_integers)
+    # if num_of_ints == 0:
+    #     return None
+
+    # if num_of_ints == 1:
+    #     return list_of_integers[0]
+
+    # if num_of_ints == 2:
+    #     if list_of_integers[0] > list_of_integers[1]:
+    #         return list_of_integers[0]
+    #     return list_of_integers[1]
+
+    # prev = list_of_integers[0]
+    # l = list_of_integers
+    # for i in range(1, num_of_ints - 2):
+    #     curr = l[i]
+    #     next = l[i + 1]
+    #     if curr >= prev and curr >= next:
+    #         return curr
+    #     prev = curr
+    # print('no!')
+
+    # max = list_of_integers[0]
+    # for int in list_of_integers:
+    #     if int > max:
+    #         max = int
+    # return max
