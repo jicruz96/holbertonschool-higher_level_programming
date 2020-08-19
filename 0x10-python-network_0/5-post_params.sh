d42ef89b88494c7cdaf587b35a5f679fc7e599a9
jicruz96 <accounts@jicruz.com>
2020-08-19 01:36:17 +0000

    Add  param to file 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # sends a POST req. to URL with an email and subject, displays body of response
-subject="I will always be here for PLD"
-email="hr@holbertonschool.com"
-curl -s -X POST -d "email=$email" -d "subject=$subject" $1
+curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" $1
