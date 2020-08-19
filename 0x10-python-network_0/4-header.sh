8023458b5c8926d0b8c6a63a147e2c20ffab5b9b
jicruz96 <accounts@jicruz.com>
2020-08-19 01:33:32 +0000

    Include  parameter in file 4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a custom header to URL and displays the body of the response
-curl -s -L -X GET -H "X-HolbertonSchool-User-Id: 98"
+curl -s -L -X GET -H "X-HolbertonSchool-User-Id: 98" $1
