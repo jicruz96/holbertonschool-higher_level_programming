73de1ef40419c9b99f48dc5a8ccd1c2c7bc6217f
jicruz96 <accounts@jicruz.com>
2020-08-28 21:17:52 +0000

    move all non-imported code in file 3 to import guard

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,9 +5,11 @@ from urllib.error import HTTPError
 from urllib.request import urlopen
 from sys import argv
 
-url = argv[1]
 
 if __name__ == "__main__":
+
+    url = argv[1]
+
     try:
         with urlopen(url) as response:
             print(response.read().decode("utf-8"))
