ca3f61bbda96d005c52c20df396816f667b71ca4
jicruz96 <accounts@jicruz.com>
2020-08-28 21:12:24 +0000

    Fix file 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """ takes in a URL, sends a request (that fails) and displays the error """
 
-from urllib import request
 from urllib.error import HTTPError
+from urllib.request import urlopen
 from sys import argv
 
 url = argv[1]
 
 if __name__ == "__main__":
     try:
-        with request.urlopen(url) as response:
-            print(response)
+        with urlopen(url) as response:
+            print(response.read().decode("utf-8"))
     except HTTPError as e:
         print("Error code: {}".format(e.code))
