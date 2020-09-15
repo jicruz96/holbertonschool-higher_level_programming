df2c300deaea6227bb7a8e977ae3a3040c93819a
jicruz96 <accounts@jicruz.com>
2020-09-15 20:55:47 +0000

    change approach for file 3: print first arg only, not at all args

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
+const arg = process.argv[2];
 
-if (args[0]) {
-  let arg;
-  for (arg of args) {
-    console.log(arg);
-  }
+if (arg) {
+  console.log(arg);
 } else {
   console.log('No argument');
 }
